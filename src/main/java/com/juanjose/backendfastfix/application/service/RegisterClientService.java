26d6353845320c2f6a450c9ca4538e75663118ab
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-03-31 16:24:43 +0200

    Modificación service y ClientPersistenceMapper

--- src/main/java/com/juanjose/backendfastfix/application/service/RegisterClientService.java
+++ src/main/java/com/juanjose/backendfastfix/application/service/RegisterClientService.java
@@ -3,7 +3,9 @@ package com.juanjose.backendfastfix.application.service;
 import com.juanjose.backendfastfix.application.port.in.RegisterClientUseCase;
 import com.juanjose.backendfastfix.application.port.out.ClientRepositoryPort;
 import com.juanjose.backendfastfix.domain.model.Client;
+import org.springframework.stereotype.Service;
 
+@Service
 public class RegisterClientService implements RegisterClientUseCase {
     private final ClientRepositoryPort clientRepositoryPort;
 
