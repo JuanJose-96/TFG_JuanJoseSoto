e81aca00e2dc442ecedfebf0895615321a9dd761
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-20 11:49:39 +0200

    Creación de data.sql con los campos de los sectores

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO sectors (name) VALUES ('Electricidad') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Fontanería') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Carpintería') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Pintura') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Cerrajería') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Climatización') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Albañilería') ON CONFLICT DO NOTHING;
+INSERT INTO sectors (name) VALUES ('Limpieza') ON CONFLICT DO NOTHING;
