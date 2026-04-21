83cf433cbcbe02b44ec65c213f77976ecc431bec
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-21 20:32:58 +0200

    Creación de domain de Technician

---
+++ src/main/java/com/juanjose/backendfastfix/domain/model/Technician.java
@@ -0,0 +1,36 @@
+package com.juanjose.backendfastfix.domain.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(toBuilder = true)
+public class Technician {
+    private Long id;
+    private String name;
+    private String surname;
+    private String email;
+    private String password;
+    private String phone;
+    private String profileImageUrl;
+    private String province;
+    private String city;
+    private String aboutMe;
+    private Long mainSectorId;
+    private String priceDescription;
+    @Builder.Default
+    private boolean emergencyAvailability = false;
+    private String scheduleAvailability;
+    @Builder.Default
+    private boolean whatsappAvailable = true;
+    @Builder.Default
+    private Double averageRating = 0.0;
+    @Builder.Default
+    private Integer totalReviews = 0;
+
+}
+
