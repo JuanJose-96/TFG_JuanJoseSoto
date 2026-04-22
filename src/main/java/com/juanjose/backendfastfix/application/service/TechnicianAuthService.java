2641a7f4ceed9b3f60089e96d40414e9fb796cf4
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-22 11:28:41 +0200

    Creacion de TechnicianAuthService

---
+++ src/main/java/com/juanjose/backendfastfix/application/service/TechnicianAuthService.java
@@ -0,0 +1,38 @@
+package com.juanjose.backendfastfix.application.service;
+
+import com.juanjose.backendfastfix.application.port.in.technician.RegisterTechnicianUseCase;
+import com.juanjose.backendfastfix.application.port.out.PasswordEncoderPort;
+import com.juanjose.backendfastfix.application.port.out.SectorRepositoryPort;
+import com.juanjose.backendfastfix.application.port.out.TechnicianRepositoryPort;
+import com.juanjose.backendfastfix.domain.exception.EmailAlreadyExists;
+import com.juanjose.backendfastfix.domain.exception.SectorNotFoundException;
+import com.juanjose.backendfastfix.domain.model.Technician;
+
+public class TechnicianAuthService implements RegisterTechnicianUseCase {
+
+    private final TechnicianRepositoryPort technicianRepositoryPort;
+    private final PasswordEncoderPort passwordEncoderPort;
+    private final SectorRepositoryPort sectorRepositoryPort;
+
+    public TechnicianAuthService(TechnicianRepositoryPort technicianRepositoryPort, PasswordEncoderPort passwordEncoderPort, SectorRepositoryPort sectorRepositoryPort) {
+        this.technicianRepositoryPort = technicianRepositoryPort;
+        this.passwordEncoderPort = passwordEncoderPort;
+        this.sectorRepositoryPort = sectorRepositoryPort;
+    }
+
+
+    @Override
+    public Technician register(Technician technician) {
+        if(technicianRepositoryPort.existsByEmail(technician.getEmail())){
+            throw new EmailAlreadyExists(technician.getEmail());
+        }
+        if(sectorRepositoryPort.exitsById(technician.getMainSectorId())) {
+            throw new SectorNotFoundException(technician.getMainSectorId());
+        }
+
+        Technician technicianToSave = technician.toBuilder()
+                .password(passwordEncoderPort.encode(technician.getPassword())).build();
+
+        return technicianRepositoryPort.save(technicianToSave);
+    }
+}
