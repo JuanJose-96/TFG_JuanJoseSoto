1fbabd9711cf59a874f8828a7d976a5785c1a778
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-22 16:38:50 +0200

    Método uploadProfileImage en TechnicianProfileService

--- src/main/java/com/juanjose/backendfastfix/application/service/technician/TechnicianProfileService.java
+++ src/main/java/com/juanjose/backendfastfix/application/service/technician/TechnicianProfileService.java
@@ -1,19 +1,25 @@
 package com.juanjose.backendfastfix.application.service.technician;
 
 import com.juanjose.backendfastfix.application.port.in.technician.UpdateTechnicianProfileUseCase;
+import com.juanjose.backendfastfix.application.port.in.technician.UploadTechnicianImageUseCase;
+import com.juanjose.backendfastfix.application.port.out.ImageStoragePort;
 import com.juanjose.backendfastfix.application.port.out.TechnicianRepositoryPort;
+import com.juanjose.backendfastfix.domain.exception.InvalidFileException;
 import com.juanjose.backendfastfix.domain.exception.TechnicianNotFoundException;
+import com.juanjose.backendfastfix.domain.model.Client;
 import com.juanjose.backendfastfix.domain.model.Technician;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class TechnicianProfileService implements UpdateTechnicianProfileUseCase {
+public class TechnicianProfileService implements UpdateTechnicianProfileUseCase, UploadTechnicianImageUseCase {
     private final TechnicianRepositoryPort technicianRepositoryPort;
+    private final ImageStoragePort imageStoragePort;
 
 
-    public TechnicianProfileService(TechnicianRepositoryPort technicianRepositoryPort) {
+    public TechnicianProfileService(TechnicianRepositoryPort technicianRepositoryPort, ImageStoragePort imageStoragePort) {
         this.technicianRepositoryPort = technicianRepositoryPort;
-
+        this.imageStoragePort = imageStoragePort;
     }
 
     @Override
@@ -35,5 +41,27 @@ public class TechnicianProfileService implements UpdateTechnicianProfileUseCase 
                 .build();
         return technicianRepositoryPort.save(technicianUpdated);
 
+    }
+
+    @Override
+    public Technician uploadProfileImage(Long id, MultipartFile file) {
+        if(file == null || file.isEmpty()){
+            throw new InvalidFileException("File is required");
+        }
+        String contentType = file.getContentType();
+        if(contentType == null || !contentType.startsWith("image/")){
+            throw new InvalidFileException("File must be an image");
+        }
+        long maxSize = 5* 1024*1024;
+        if(file.getSize() > maxSize){
+            throw new InvalidFileException("Image must be less than 5MB");
+        }
+
+        Technician technician = technicianRepositoryPort.findById(id)
+                .orElseThrow(() -> new TechnicianNotFoundException(id));
+
+        String imageUrl = imageStoragePort.uploadImage(file);
+        Technician technicianImage = technician.toBuilder().profileImageUrl(imageUrl).build();
+        return technicianRepositoryPort.save(technicianImage);
     }
 }
