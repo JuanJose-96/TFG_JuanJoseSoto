797c4a3a967cd5ff37b2c161bd71dba18bee0d31
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-23 21:40:17 +0200

    TechnicianProfileController subir imagen en Cloudinary

--- src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/controller/technician/TechnicianProfileController.java
+++ src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/controller/technician/TechnicianProfileController.java
@@ -1,6 +1,7 @@
 package com.juanjose.backendfastfix.infrastructure.adapter.in.rest.controller.technician;
 
 import com.juanjose.backendfastfix.application.port.in.technician.UpdateTechnicianProfileUseCase;
+import com.juanjose.backendfastfix.application.port.in.technician.UploadTechnicianImageUseCase;
 import com.juanjose.backendfastfix.domain.model.Technician;
 import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.technician.TechnicianResponse;
 import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.technician.UpdateTechnicianProfileRequest;
@@ -8,14 +9,17 @@ import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.mapper.Technic
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/technician/profile")
 public class TechnicianProfileController {
     private final UpdateTechnicianProfileUseCase updateTechnicianProfileUseCase;
+    private final UploadTechnicianImageUseCase uploadTechnicianImageUseCase;
 
-    public TechnicianProfileController(UpdateTechnicianProfileUseCase updateTechnicianProfileUseCase) {
+    public TechnicianProfileController(UpdateTechnicianProfileUseCase updateTechnicianProfileUseCase, UploadTechnicianImageUseCase uploadTechnicianImageUseCase) {
         this.updateTechnicianProfileUseCase = updateTechnicianProfileUseCase;
+        this.uploadTechnicianImageUseCase = uploadTechnicianImageUseCase;
     }
 
     @PutMapping("/{id}")
@@ -24,6 +28,13 @@ public class TechnicianProfileController {
         Technician technicianUpdated = updateTechnicianProfileUseCase.updateProfile(id, TechnicianRestMapper.updateProfileToDomain(request));
 
         return ResponseEntity.ok(TechnicianRestMapper.fromDomain(technicianUpdated));
+    }
+
+    @PostMapping("/{id}/image")
+    public ResponseEntity<TechnicianResponse> uploadImage(@PathVariable Long id,
+                                                          @RequestParam("technicianImage") MultipartFile file){
+        Technician technician = uploadTechnicianImageUseCase.uploadProfileImage(id,file);
+        return ResponseEntity.ok(TechnicianRestMapper.fromDomain(technician));
 
     }
 }
