87254ac4ae68ebef3cdebae1165dd8d888262458
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-04-26 22:36:27 +0200

    Creacion de ReviewService

---
+++ src/main/java/com/juanjose/backendfastfix/application/service/ReviewService.java
@@ -0,0 +1,91 @@
+package com.juanjose.backendfastfix.application.service;
+
+import com.juanjose.backendfastfix.application.port.in.review.GetClientReviewsUseCase;
+import com.juanjose.backendfastfix.application.port.in.review.GetTechnicianReviewsUseCase;
+import com.juanjose.backendfastfix.application.port.in.review.PublishReviewUseCase;
+import com.juanjose.backendfastfix.application.port.out.ClientRepositoryPort;
+import com.juanjose.backendfastfix.application.port.out.ReviewRepositoryPort;
+import com.juanjose.backendfastfix.application.port.out.TechnicianRepositoryPort;
+import com.juanjose.backendfastfix.domain.exception.ClientNotFoundException;
+import com.juanjose.backendfastfix.domain.exception.InvalidRatingException;
+import com.juanjose.backendfastfix.domain.exception.ReviewAlreadyExistsException;
+import com.juanjose.backendfastfix.domain.exception.TechnicianNotFoundException;
+import com.juanjose.backendfastfix.domain.model.Review;
+import com.juanjose.backendfastfix.domain.model.Technician;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Service
+public class ReviewService implements PublishReviewUseCase, GetClientReviewsUseCase, GetTechnicianReviewsUseCase {
+    private final ClientRepositoryPort clientRepositoryPort;
+    private final ReviewRepositoryPort reviewRepositoryPort;
+    private final TechnicianRepositoryPort technicianRepositoryPort;
+
+    public ReviewService(ClientRepositoryPort clientRepositoryPort, ReviewRepositoryPort reviewRepositoryPort, TechnicianRepositoryPort technicianRepositoryPort) {
+        this.clientRepositoryPort = clientRepositoryPort;
+        this.reviewRepositoryPort = reviewRepositoryPort;
+        this.technicianRepositoryPort = technicianRepositoryPort;
+    }
+
+    @Override
+    public List<Review> getReviewsByClientId(Long clientId) {
+        clientRepositoryPort.findById(clientId)
+                .orElseThrow(() -> new ClientNotFoundException(clientId));
+
+        return reviewRepositoryPort.findByClientId(clientId);
+
+    }
+
+    @Override
+    public List<Review> getReviewsByTechnicianId(Long technicianId) {
+        technicianRepositoryPort.findById(technicianId)
+                .orElseThrow(() -> new TechnicianNotFoundException(technicianId));
+
+        return reviewRepositoryPort.findByTechnicianId(technicianId);
+
+    }
+
+    @Override
+    public Review publish(Long clientId, Long technicianId, int rating, String comment) {
+        if(rating < 1 || rating > 5){
+            throw new InvalidRatingException();
+        }
+        clientRepositoryPort.findById(clientId)
+                .orElseThrow(() -> new ClientNotFoundException(clientId));
+
+        Technician technician = technicianRepositoryPort.findById(technicianId)
+                .orElseThrow(() -> new TechnicianNotFoundException(technicianId));
+
+        if(reviewRepositoryPort.existsByClientAndTechnicianId(clientId,technicianId)){
+            throw new ReviewAlreadyExistsException();
+        }
+        Review review = Review.builder()
+                .clientId(clientId)
+                .technicianId(technicianId)
+                .rating(rating)
+                .comment(comment)
+                .createdAt(LocalDateTime.now()).build();
+
+        updateTechnicianRating(technician);
+        return reviewRepositoryPort.save(review);
+
+    }
+
+    private void updateTechnicianRating(Technician technician){
+        List<Review> reviewsTechnician = reviewRepositoryPort.findByTechnicianId(technician.getId());
+
+        double rating = reviewsTechnician.stream()
+                .mapToInt(review -> review.getRating())
+                .average().orElse(0.0);
+
+        Technician updateTechnician = technician.toBuilder()
+                .averageRating(rating)
+                .totalReviews(reviewsTechnician.size())
+                .build();
+
+        technicianRepositoryPort.save(updateTechnician);
+
+    }
+}
