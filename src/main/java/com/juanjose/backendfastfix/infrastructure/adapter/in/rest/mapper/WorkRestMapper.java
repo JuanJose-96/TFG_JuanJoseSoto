b5dbb0c5e9d77a25a4d70c9dbbc61ad0346e0d0a
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-05-11 23:20:41 +0200

    Creacion de mapper rest de work

---
+++ src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/mapper/WorkRestMapper.java
@@ -0,0 +1,36 @@
+package com.juanjose.backendfastfix.infrastructure.adapter.in.rest.mapper;
+
+import com.juanjose.backendfastfix.domain.model.Work;
+import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work.CreateWorkRequest;
+import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work.WorkResponse;
+
+public class WorkRestMapper {
+    public static Work createToDomain(CreateWorkRequest request){
+        return Work.builder()
+                .technicianId(request.technicianId())
+                .clientId(request.clientId())
+                .clientName(request.clientName())
+                .clientSurname(request.clientSurname())
+                .clientProvince(request.clientProvince())
+                .clientCity(request.clientCity())
+                .serviceDate(request.serviceDate())
+                .totalAmount(request.totalAmount())
+                .serviceDescription(request.serviceDescription())
+                .build();
+    }
+
+    public static WorkResponse fromDomain(Work work){
+        return new WorkResponse(
+                work.getId(),
+                work.getTechnicianId(),
+                work.getClientId(),
+                work.getClientName(),
+                work.getClientSurname(),
+                work.getClientProvince(),
+                work.getClientCity(),
+                work.getServiceDate(),
+                work.getTotalAmount(),
+                work.getServiceDescription()
+        );
+    }
+}
