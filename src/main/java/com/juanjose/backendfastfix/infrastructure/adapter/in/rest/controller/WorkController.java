b9a5fe6bff1812ee24d880405eae9bd893500f83
JuanJose-96 <jjsaVIXXXIII@gmail.com>
2026-05-12 09:03:02 +0200

    Actualizacion endpoints en WorkController

--- src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/controller/WorkController.java
+++ src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/controller/WorkController.java
@@ -1,25 +1,34 @@
 package com.juanjose.backendfastfix.infrastructure.adapter.in.rest.controller;
 
 import com.juanjose.backendfastfix.application.port.in.work.CreateAWorkUseCase;
+import com.juanjose.backendfastfix.application.port.in.work.DeleteWorkUseCase;
+import com.juanjose.backendfastfix.application.port.in.work.EditWorkUseCase;
+import com.juanjose.backendfastfix.application.port.in.work.GetTechnicianWorksUseCase;
 import com.juanjose.backendfastfix.domain.model.Work;
 import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work.CreateWorkRequest;
+import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work.EditWorkRequest;
 import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work.WorkResponse;
 import com.juanjose.backendfastfix.infrastructure.adapter.in.rest.mapper.WorkRestMapper;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/works")
 public class WorkController {
     private final CreateAWorkUseCase createAWorkUseCase;
+    private final GetTechnicianWorksUseCase getTechnicianWorksUseCase;
+    private final EditWorkUseCase editWorkUseCase;
+    private final DeleteWorkUseCase deleteWorkUseCase;
 
-    public WorkController(CreateAWorkUseCase createAWorkUseCase) {
+    public WorkController(CreateAWorkUseCase createAWorkUseCase, GetTechnicianWorksUseCase getTechnicianWorksUseCase, EditWorkUseCase editWorkUseCase, DeleteWorkUseCase deleteWorkUseCase) {
         this.createAWorkUseCase = createAWorkUseCase;
+        this.getTechnicianWorksUseCase = getTechnicianWorksUseCase;
+        this.editWorkUseCase = editWorkUseCase;
+        this.deleteWorkUseCase = deleteWorkUseCase;
     }
 
     @PostMapping
@@ -32,4 +41,32 @@ public class WorkController {
                 .body(WorkRestMapper.fromDomain(workSaved));
 
     }
+
+    @GetMapping("/technician/{technicianId}")
+    public ResponseEntity<List<WorkResponse>> getWorksByTechnician(@PathVariable Long technicianId){
+        List<WorkResponse> works = getTechnicianWorksUseCase.getByTechnicianId(technicianId)
+                .stream()
+                .map(WorkRestMapper::fromDomain)
+                .toList();
+
+        return ResponseEntity.ok(works);
+    }
+
+    @DeleteMapping("/{workId}")
+    public ResponseEntity<Void> deleteWork(@PathVariable Long workId){
+        deleteWorkUseCase.delete(workId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping("/{workId}")
+    public ResponseEntity<WorkResponse> editWork(@PathVariable Long workId,
+                                                 @Valid @RequestBody EditWorkRequest request){
+
+        Work updatedWork = editWorkUseCase
+                .edit(workId, WorkRestMapper.editToDomain(request));
+
+        return ResponseEntity.ok(WorkRestMapper.fromDomain(updatedWork));
+    }
+
+
 }
--- src/main/java/com/juanjose/backendfastfix/infrastructure/adapter/in/rest/dto/work/DeleteWorkRequest.java
+++
@@ -1,9 +0,0 @@
-package com.juanjose.backendfastfix.infrastructure.adapter.in.rest.dto.work;
-
-import jakarta.validation.constraints.NotNull;
-
-public record DeleteWorkRequest(
-        @NotNull(message = "Technician id is required")
-        Long technicianId
-) {
-}
